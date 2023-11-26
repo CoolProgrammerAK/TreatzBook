bcfe86da83eb425ca5728062eb274eca2c2563f9
CoolProgrammerAK <avineykhetarpal01@gmail.com>
2023-11-25 23:56:28 -0500

    styling + dashboard UI

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,12 +2,12 @@ import React, { lazy, useEffect } from "react";
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 // import Booking from "../components/Dashboard/Booking";
-// import DashboardContainer from "../containers/DashboardContainer";
+import DashboardContainer from "../containers/DashboardContainer";
 // import { loadUser } from "../redux/actions/auth";
 import { store } from "../redux/store/store";
 import setAuthToken from "../utils/setAuthToken";
 import PrivateRoute from "./PrivateRoute";
-// import AppointmentContainer from "../containers/AppointmentContainer";
+import AppointmentContainer from "../containers/AppointmentContainer";
 
 const LandingContainer = lazy(() => import("../containers/LandingContainer"));
 const LoginContainer = lazy(() => import("../containers/auth/LoginContainer"));
@@ -37,13 +37,13 @@ console.log(location)
       <Route exact path="/" element={<LandingContainer></LandingContainer>} />
       <Route exact path="/login" element={<LoginContainer></LoginContainer>} />
       <Route exact path="/register" element={<RegisterContainer></RegisterContainer>} />
-      {/* <PrivateRoute exact path="/dashboard" component={DashboardContainer} /> */}
-      {/* <PrivateRoute exact path="/doctor/:id" component={Booking} /> */}
-      {/* <PrivateRoute
+      <Route exact path="/dashboard" element={<DashboardContainer></DashboardContainer>} />
+      {/* <Route exact path="/doctor/:id" component={Booking} /> */}
+      <Route
         exact
         path="/appointments"
-        component={AppointmentContainer}
-      /> */}
+       element={<AppointmentContainer></AppointmentContainer>}
+      />
       <Route path="*" exact element={<NotFoundContainer></NotFoundContainer>} />
       {/* <Navigate to="/404" /> */}
     </Routes>
